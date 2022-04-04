5a3a5a22c11428e81c4ef0eb87f54b4622300aa1
the-eric-kwok <erickwok404@gmail.com>
2022-04-04 15:47:38 +0800

    1. 修复了顶层 await 问题 2. 在输出文件末尾加上换行符

--- main.js
+++ main.js
@@ -8,60 +8,62 @@ const readFromFile = require('./src/readFromFile');
 const writeToFile = require('./src/writeToFile');
 const unicodeReplace = require('./src/unicodeReplace');
 
-let filePath;
-if (process.argv.length <= 2) {
-    console.error("未传入要混淆的文件");
-    printHelp();
-    process.exit(1);
-}
-for (let index = 2; index < process.argv.length; index++) {
-    if (process.argv[index] === "-h" || process.argv[index] === "--help" || process.argv[index] === "help") {
+(async function () {
+    let filePath;
+    if (process.argv.length <= 2) {
+        console.error("未传入要混淆的文件");
         printHelp();
         process.exit(1);
     }
-    if (process.argv[index].includes(".js")) {
-        filePath = process.argv[index];
+    for (let index = 2; index < process.argv.length; index++) {
+        if (process.argv[index] === "-h" || process.argv[index] === "--help" || process.argv[index] === "help") {
+            printHelp();
+            process.exit(1);
+        }
+        if (process.argv[index].includes(".js")) {
+            filePath = process.argv[index];
+        }
     }
-}
-if (!filePath) {
-    console.error("未传入要混淆的 js 文件");
-    printHelp();
-    process.exit(1);
-}
+    if (!filePath) {
+        console.error("未传入要混淆的 js 文件");
+        printHelp();
+        process.exit(1);
+    }
 
-let orig_code = await readFromFile(filePath);
-if (orig_code.replace(/(^s*)|(s*$)/g, "").length === 0) {
-    console.error("输入的文件为空或读取失败");
-    process.exit(1);
-}
-var obfuscationResult = JavascriptObfuscator.obfuscate(orig_code, {
-    compact: true,
-    controlFlowFlattening: true,
-    controlFlowFlatteningThreshold: 1,
-    unicodeEscapeSequence: true,
-    deadCodeInjection: true,
-    deadCodeInjectionThreshold: 1,
-    debugProtection: true,
-    debugProtectionInterval: 1000,
-    splitStrings: true,
-    splitStringsChunkLength: 5,
-    stringArrayIndexShift: true,
-    stringArrayRotate: true,
-    stringArrayShuffle: true,
-    stringArrayWrappersType: "function",
-    stringArrayThreshold: 1,
-    simplify: true,
-    transformObjectKeys: true,
-    renameGlobals: true,
-    disableConsoleOutput: false,
-    stringArrayEncoding: ["rc4"],
-    identifierNamesGenerator: "dictionary",
-    identifiersDictionary: identifierDictGenerator(1000),
-});
-let obfuscatedFilePath = path.dirname(filePath) + path.sep + path.basename(filePath).split(".").map((item, index) => {
-    if (index === 0) {
-        return item + "-obfuscated";
+    let orig_code = await readFromFile(filePath);
+    if (orig_code.replace(/(^s*)|(s*$)/g, "").length === 0) {
+        console.error("输入的文件为空或读取失败");
+        process.exit(1);
     }
-    return item;
-}).join(".");
-writeToFile(obfuscatedFilePath, unicodeReplace(obfuscationResult.getObfuscatedCode()));
+    var obfuscationResult = JavascriptObfuscator.obfuscate(orig_code, {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 1,
+        unicodeEscapeSequence: true,
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 1,
+        debugProtection: true,
+        debugProtectionInterval: 1000,
+        splitStrings: true,
+        splitStringsChunkLength: 5,
+        stringArrayIndexShift: true,
+        stringArrayRotate: true,
+        stringArrayShuffle: true,
+        stringArrayWrappersType: "function",
+        stringArrayThreshold: 1,
+        simplify: true,
+        transformObjectKeys: true,
+        renameGlobals: true,
+        disableConsoleOutput: false,
+        stringArrayEncoding: ["rc4"],
+        identifierNamesGenerator: "dictionary",
+        identifiersDictionary: identifierDictGenerator(1000),
+    });
+    let obfuscatedFilePath = path.dirname(filePath) + path.sep + path.basename(filePath).split(".").map((item, index) => {
+        if (index === 0) {
+            return item + "-obfuscated";
+        }
+        return item;
+    }).join(".");
+    writeToFile(obfuscatedFilePath, unicodeReplace(obfuscationResult.getObfuscatedCode()) + "\n");
+})()
